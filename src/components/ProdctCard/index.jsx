f0686aeb6313fce363ece49817e91848de0d8d44
CodeX1315 <deepakmahade777@gmail.com>
2025-09-24 19:16:09 +0530

    Auth JWT

--- src/components/ProdctCard/index.jsx
+++ src/components/ProdctCard/index.jsx
@@ -2,29 +2,34 @@ import { useCart } from "../../context/cartd-context";
 import { findProductInCart } from "../../utils/findProductInCart";
 import { useNavigate } from "react-router-dom";
 import { findFavProduct } from "../../utils/findFavProduct";
-export const ProductCard = ({product}) => {
-
+export const ProductCard = ({ product }) => {
   const { cartDispatch, cart, fav } = useCart();
-  const isProductInCart = findProductInCart( cart, product.id);
-  const isProductInFav = findFavProduct( fav, product.id );
+  const isProductInCart = findProductInCart(cart, product.id);
+  const isProductInFav = findFavProduct(fav, product.id);
   const navigate = useNavigate();
   // console.log(isProductInCart);
   const onCartClick = (product) => {
-        !isProductInCart ?
-       cartDispatch({
+    if (!isProductInCart) {
+      localStorage.setItem('cart', JSON.stringify([...cart,product]))
+      cartDispatch({
         type: "ADD_TO_CART",
-        payload: { product }
-      }) : navigate('/cart')
-  }
+        payload: { product },
+      });
+    } else {
+      navigate("/cart");
+    }
+  };
 
   // console.log(cart);
   const onFavClick = (product) => {
-     !isProductInFav ?
+    if(!isProductInFav){
+      localStorage.setItem('fav', JSON.stringify([...fav,product]))
       cartDispatch({
-        type: "ADD_TO_FAV",
-        payload: { product }
-      }) : navigate('/favorite')
-  }
+          type: "ADD_TO_FAV",
+          payload: { product },
+        })
+     } else { navigate("/favorite"); }
+  };
 
   return (
     <div className="card card-vertical d-flex direction-column relative shadow">
@@ -34,37 +39,32 @@ export const ProductCard = ({product}) => {
       <div className="card-details">
         <div className="card-title">{product.title}</div>
         <div className="card-description">
-            <p>{product.category.name}</p>
+          <p>{product.category.name}</p>
           <p className="card-price">
             Rs. {product.price}00
             {/* <span className="price-strike-through">Rs. 2499</span> */}
             <span className="discount">(30% OFF)</span>
           </p>
         </div>
-        <div  className="cta-btn">
-            <button onClick={() => onFavClick(product)} className="button btn-primary btn-icon cart-btn d-flex bg-rose-400 align-center justify-center gap cursor btn-margin">
-             <span className="material-icons-outlined text-3xl flex">
-                {
-                    isProductInFav ? 'favorite' : 'favorite_border'
-                }
-             </span>
-             {
-                isProductInFav ? 'Check Wishlist' : 'Add to wishlist'
-             }
+        <div className="cta-btn">
+          <button
+            onClick={() => onFavClick(product)}
+            className="button btn-primary btn-icon cart-btn d-flex bg-rose-400 align-center justify-center gap cursor btn-margin"
+          >
+            <span className="material-icons-outlined text-3xl flex">
+              {isProductInFav ? "favorite" : "favorite_border"}
+            </span>
+            {isProductInFav ? "Check Wishlist" : "Add to wishlist"}
           </button>
-          <button onClick={ () => onCartClick(product)} className="button btn-primary btn-icon cart-btn d-flex bg-rose-400  align-center justify-center gap cursor btn-margin">
-             <span className="material-icons-outlined text-3xl">
-              {
-                isProductInCart ? 'shopping_cart_checkout': 'shopping_cart'
-              }
-            
-             </span>
-            {
-              isProductInCart ? 'Go To Cart' : 'Add To Cart'
-             }
-             
+          <button
+            onClick={() => onCartClick(product)}
+            className="button btn-primary btn-icon cart-btn d-flex bg-rose-400  align-center justify-center gap cursor btn-margin"
+          >
+            <span className="material-icons-outlined text-3xl">
+              {isProductInCart ? "shopping_cart_checkout" : "shopping_cart"}
+            </span>
+            {isProductInCart ? "Go To Cart" : "Add To Cart"}
           </button>
-          
         </div>
       </div>
     </div>
